3357f944a4f90752ba0247d1088d7fb50b04ffd2
dahjah <dahjah@gmail.com>
2017-05-12 12:31:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
 var express = require('express');
 var request = require('request');
-var parseString = require('xml2js').parseString;
+var cors = require('cors');
+var xml2js = require('xml2js');
 
 var app = express();
+app.use(cors());
 module.exports.app = app;
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-
-  var url = req.query.url;
-  if (!url) {
-    res.status(422);
-    return res.json({message: 'url parameter not found'});
-  }
+  var count = req.query.count || 15;
+  var url = 'https://groups.google.com/forum/feed/cugos/topics/rss.xml?num='+count;
 
   request(url, function(err, response, body) {
+    if (err) {
+      res.status(err.code || 400);
+      return res.json({message: 'error fetching url', error: err.message});
+    }
+
+    if (response.statusCode !== 200) {
+      res.status(response.statusCode);
+      return res.json({message: 'unable to fetch url', code: response.statusCode});
+    }
+
+    var parser = new xml2js.Parser({
+      explicitArray: false
+    });
+
+    parser.parseString(body, function (err, result) {
       if (err) {
         res.status(err.code || 400);
-        return res.json({message: 'error fetching url', error: err.message});
+        return res.json({message: 'unable to parse XML'});
       }
 
-      console.log(body);
-
-      if (response.statusCode !== 200) {
-        res.status(response.statusCode);
-        return res.json({message: 'unable to fetch url', code: response.statusCode});
-      }
-
-      parseString(body, function (err, result) {
-        if (err) {
-          res.status(err.code || 400);
-          return res.json({message: 'unable to parse XML'});
-        }
-
-        return res.json({url: url, feed: result});
-      });
-
-
+      return res.json(result);
+    });
   });
-
 });
 
 app.listen(app.get('port'), function() {
