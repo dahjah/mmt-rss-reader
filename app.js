eff6aa3dccd4db47e11d6a03a9bf38d051bcd3bb
dahjah <dahjah@gmail.com>
2017-05-12 17:44:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,44 @@ app.use(cors());
 module.exports.app = app;
 app.set('port', (process.env.PORT || 5000));
 
+/*FeedParser Test Code*/
+app.get('/feedparser', function(req1,res1){
+var url = req1.query.url;
+var req = request(url);
+var feedparser = new FeedParser([options]);
+
+req.on('error', function (error) {
+  console.log('error1');
+});
+
+req.on('response', function (res) {
+  var stream = this; // `this` is `req`, which is a stream
+
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error('Bad status code'));
+  }
+  else {
+    stream.pipe(feedparser);
+  }
+});
+
+feedparser.on('error', function (error) {
+  console.log('error2');
+});
+
+feedparser.on('readable', function () {
+  // This is where the action is!
+  var stream = this; // `this` is `feedparser`, which is a stream
+  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+  var item;
+
+  while (item = stream.read()) {
+    console.log(item);
+  }
+});
+});
+/*END FeedParser Test Code*/
+
 app.get('/', function(req, res) {
   var count = req.query.count || 15;
   var url = req.query.url;
