f4f20651bc2d366b2932f14ed77d72f07848904d
dahjah <dahjah@gmail.com>
2017-05-12 17:13:30 -0600

    Added Node-Feedparser require statement

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var request = require('request');
 var cors = require('cors');
 var xml2js = require('xml2js');
+var parser = require('node-feedparser');
 
 var app = express();
 app.use(cors());
