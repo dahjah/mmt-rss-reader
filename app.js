c17ca34ec93bfababc9897e75d670faaf147a3ec
dahjah <dahjah@gmail.com>
2017-05-12 18:23:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -39,11 +39,12 @@ feedparser.on('readable', function () {
   var stream = this; // `this` is `feedparser`, which is a stream
   var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
   var item;
-
+  var response;
   while (item = stream.read()) {
     console.log(item);
+    response.push(item);
   }
-  res1.send(meta);
+  res1.send(response);
 });
 });
 /*END FeedParser Test Code*/
