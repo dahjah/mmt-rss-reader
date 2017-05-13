46d01807f48c7ac83e2bf071dfcc0ceafafcb999
dahjah <dahjah@gmail.com>
2017-05-12 18:41:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/feedparser', function(req1,res1){
 var url = req1.query.url;
 var req = request(url);
-var feedparser = new FeedParser([options]);
+var feedparser = new FeedParser();
 
 req.on('error', function (error) {
   console.log('error1');
@@ -39,7 +39,7 @@ feedparser.on('readable', function () {
   var stream = this; // `this` is `feedparser`, which is a stream
   var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
   var item;
-
+console.log('-------------------------------------');
   while (item = stream.read()) {
     console.log(item);
   }
